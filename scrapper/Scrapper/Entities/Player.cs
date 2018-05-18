b7d441500b2e64d36da7153dc3ec81c669991b51
Greaka <mrgreaka@gmail.com>
2018-05-19 01:07:15 +0200

    you can dodge now

--- scrapper/Scrapper/Entities/Player.cs
+++ scrapper/Scrapper/Entities/Player.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Diagnostics;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using scrapper.Scrapper.Animation;
@@ -9,9 +8,11 @@ namespace scrapper.Scrapper.Entities
     internal class Player : AnimatedSprite
     {
         private const float MoveSpeed = 200.5f;
+        private const float DodgeDistance = 30f;
 
         // ReSharper disable once InconsistentNaming
         private const float TOLERANCE = 0.001f;
+        private bool _dodged;
         private Vector2 _position;
 
         public Player(Game game) : base(game, 32, 32, 4, TimeSpan.FromMilliseconds(100), "placeholder")
@@ -22,9 +23,12 @@ namespace scrapper.Scrapper.Entities
         public override void Update(GameTime gameTime)
         {
             var direction = Vector2.Zero;
-            if (GamePad.GetState(PlayerIndex.One).IsConnected)
+            var dodging = false;
+            var pad = GamePad.GetState(PlayerIndex.One);
+            if (pad.IsConnected)
             {
-                direction = GamePad.GetState(PlayerIndex.One).ThumbSticks.Left;
+                direction = pad.ThumbSticks.Left;
+                if (pad.IsButtonDown(Buttons.X)) dodging = true;
             }
             else
             {
@@ -32,6 +36,25 @@ namespace scrapper.Scrapper.Entities
                 if (Keyboard.GetState().IsKeyDown(Keys.S)) direction.Y += 1;
                 if (Keyboard.GetState().IsKeyDown(Keys.A)) direction.X -= 1;
                 if (Keyboard.GetState().IsKeyDown(Keys.D)) direction.X += 1;
+                if (Keyboard.GetState().IsKeyDown(Keys.Space)) dodging = true;
+            }
+
+            if (direction.LengthSquared() > 1)
+            {
+                direction.Normalize();
+            }
+
+            if (!_dodged)
+            {
+                if (dodging)
+                {
+                    _dodged = true;
+                    direction *= DodgeDistance;
+                }
+            }
+            else
+            {
+                if (!dodging) _dodged = false;
             }
 
             if (Math.Abs(direction.X) < TOLERANCE)
